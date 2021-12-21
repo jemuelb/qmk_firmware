edc4fd8fb32247eee3cecf7c352bca2148f350cc
Jemuel Balangcod <jemuelb@gmail.com>
2021-12-21 09:44:17 -0800

    Removed space cadet shift

--- keyboards/system76/launch_1/keymaps/jemuel-main/keymap.c
+++ keyboards/system76/launch_1/keymaps/jemuel-main/keymap.c
@@ -17,7 +17,7 @@ ________________________________________________________________________________
   |    MO(1)   |   A    |   S    |   D    |   F    |   G    |   H    |   J    |   K    |   L    |   :    |   "    |   ENTER    |   |  END   |
   |____________|________|________|________|________|________|________|________|________|________|________|________|____________|___|________|
   |                |        |        |        |        |        |        |        |   ,    |    .   |   /    |            |        |
-  |     LSPO       |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |   <    |    >   |   ?    |   RSPC     |   UP   |
+  |     SHIFT      |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |   <    |    >   |   ?    |   SHIFT    |   UP   |
   |________________|________|________|________|________|________|________|________|________|________|________|____________|________|_________
   |            |        |       |        |                 |                 |        |        |             |   |        |        |        |
   |    CTRL    |  LGUI  | MO(1) | LALT   |    BACKSPACE    |      SPACE      | RCTRL  |  RALT  |    MO(2)    |   |  LEFT  |  DOWN  | RIGHT  |
@@ -29,7 +29,7 @@ ________________________________________________________________________________
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_PGUP,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      KC_PGDN,
         MO(1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,           KC_END,
-        KC_LSPO,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSPC,    KC_UP,
+        KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,    KC_UP,
         KC_LCTL, KC_LGUI, MO(1),   KC_LALT,      KC_BSPC,          KC_SPC,     KC_RCTL,   KC_RALT,   MO(2),         KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
