7449f4a135900dd7aca8f34029177482a005b02e
Jemuel Balangcod <jemuelb@gmail.com>
2021-12-23 10:00:27 -0800

    Changed layers

--- keyboards/system76/launch_1/keymaps/jemuel-main/keymap.c
+++ keyboards/system76/launch_1/keymaps/jemuel-main/keymap.c
@@ -36,7 +36,7 @@ ________________________________________________________________________________
   |     SHIFT      |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |   <    |    >   |   ?    |   SHIFT    |   UP   |
   |________________|________|________|________|________|________|________|________|________|________|________|____________|________|_________
   |            |        |       |        |                 |                 |        |        |             |   |        |        |        |
-  |    CTRL    |  LGUI  | MO(1) | LALT   |    BACKSPACE    |      SPACE      | RCTRL  |  RALT  |    MO(2)    |   |  LEFT  |  DOWN  | RIGHT  |
+  |    CTRL    |  LGUI  | MO(1) | LALT   |    BACKSPACE    |      SPACE      | RCTRL  |  RALT  |    MO(1)    |   |  LEFT  |  DOWN  | RIGHT  |
   |____________|________|_______|________|_________________|_________________|________|________|_____________|   |________|________|________|
 */
 
@@ -46,7 +46,7 @@ ________________________________________________________________________________
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      KC_PGDN,
         MO(1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,           KC_END,
         KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,    KC_UP,
-        KC_LCTL, KC_LGUI, MO(1),   KC_LALT,      KC_BSPC,          KC_SPC,     KC_RCTL,   KC_RALT,   MO(2),         KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, MO(1),   KC_LALT,      KC_BSPC,          KC_SPC,     KC_RCTL,   KC_RALT,   MO(1),         KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
     /* Layer 1, function layer
@@ -67,7 +67,7 @@ ________________________________________________________________________________
   |                |        |        |        |        |        |        |        |        |        |        |            |        |
   |________________|________|________|________|________|________|________|________|________|________|________|____________|________|_________
   |            |        |       |        |                 |                 |        |        |             |   |        |        |        |
-  |            |        |       |        |      DELETE     |   PLAY/PAUSE    |        |        |             |   |        |        |        |
+  |            |        |       |        |      DELETE     |   PLAY/PAUSE    |        |        |    MO(2)    |   |        |        |        |
   |____________|________|_______|________|_________________|_________________|________|________|_____________|   |________|________|________|
 
 * 'RESET' resets the controller and puts the board into firmware flashing mode. If this key is hit accidentally, just unplug the board
@@ -80,7 +80,7 @@ ________________________________________________________________________________
         KC_CAPS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,    KC_TRNS,     KC_TRNS,
               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS,
-          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_DEL,          KC_MPLY,      KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,  KC_TRNS, KC_TRNS
+          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_DEL,          KC_MPLY,      KC_TRNS, KC_TRNS, MO(2),          KC_TRNS,  KC_TRNS, KC_TRNS
   ),
 
     /* Layer 2, function layer
