77525f34f89e1d7ee6ae6bedb6b0d28f1d3e82f2
Jemuel Balangcod <jemuelb@gmail.com>
2021-12-26 05:02:27 -0800

    Formatting updates

--- keyboards/system76/launch_1/keymaps/jemuel-main/keymap.c
+++ keyboards/system76/launch_1/keymaps/jemuel-main/keymap.c
@@ -36,7 +36,7 @@ ________________________________________________________________________________
   |     SHIFT      |   Z    |   X    |   C    |   V    |   B    |   N    |   M    |   <    |    >   |   ?    |   SHIFT    |   UP   |
   |________________|________|________|________|________|________|________|________|________|________|________|____________|________|_________
   |            |        |       |        |                 |                 |        |        |             |   |        |        |        |
-  |    CTRL    |  LGUI  | MO(1) | LALT   |    BACKSPACE    |      SPACE      | RCTRL  |  RALT  |    MO(1)    |   |  LEFT  |  DOWN  | RIGHT  |
+  |    CTRL    |  LGUI  | MO(1) | LALT   |    LSFT/BSPC    |      SPACE      | RCTRL  |  RALT  |    MO(1)    |   |  LEFT  |  DOWN  | RIGHT  |
   |____________|________|_______|________|_________________|_________________|________|________|_____________|   |________|________|________|
 */
 
@@ -49,20 +49,22 @@ ________________________________________________________________________________
         KC_LCTL, KC_LGUI, MO(2),   KC_LALT,    LSFT_T(KC_BSPC),    KC_SPC,     KC_RCTL,   KC_RALT,   MO(2),         KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
-    // Another base layer (for gaiming)
+    /*
+     * Another base layer, removes the mod-tap keys
+     */
   [1] = LAYOUT(
-      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,       KC_HOME,
-      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_PGUP,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_PGDN,
-      MO(2), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,       KC_END,
-      KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,    KC_UP,
-        KC_LCTL, KC_LGUI, MO(2),   KC_LALT,    KC_BSPC,    KC_SPC,     KC_RCTL,   KC_RALT,   MO(2),         KC_LEFT, KC_DOWN, KC_RGHT
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,       KC_HOME,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_PGUP,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      KC_PGDN,
+        MO(2),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,          KC_END,
+        KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,    KC_RSFT,    KC_UP,
+        KC_LCTL, KC_LGUI, MO(2),   KC_LALT,      KC_BSPC,       KC_SPC,         KC_RCTL,   KC_RALT,   MO(2),         KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
     /* Layer 2, function layer
 __________________________________________________________________________________________________________________________________  ________
 |        |        |        |        |        |        |        |        |        |        |        |        |        |            ||        |
-|        |        |        |        |        |        |        |        |        |        |        |        |        |   INSERT   ||        |
+|        |  DF(0) |  DF(1) |        |        |        |        |        |        |        |        |        |        |   INSERT   ||        |
 |________|________|________|________|________|________|________|________|________|________|________|________|________|____________||________|
 |        |        |        |        |        |        |        |        |        |        |        |        |        |            ||        |
 |        |        |        |        |        |        |        |        |        |        |        |        |        |            ||        |
@@ -87,10 +89,10 @@ ________________________________________________________________________________
   [2] = LAYOUT(
     KC_TRNS,   DF(0),   DF(1),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS,    KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
-        KC_CAPS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,    KC_TRNS,     KC_TRNS,
-              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,  KC_TRNS,
-          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_DEL,          KC_MPLY,      KC_TRNS, KC_TRNS, MO(2),          KC_TRNS,  KC_TRNS, KC_TRNS
+    KC_CAPS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS, KC_TRNS,    KC_TRNS,       KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,      KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_DEL,          KC_MPLY,      KC_TRNS, KC_TRNS, MO(2),          KC_TRNS,  KC_TRNS, KC_TRNS
   ),
 
     /* Layer 3, function layer
