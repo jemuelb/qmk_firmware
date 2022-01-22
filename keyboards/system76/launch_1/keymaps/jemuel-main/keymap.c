5406030b8395f9b493fae4b474d8dd91ab892f0c
Jemuel Balangcod <jemuelb@gmail.com>
2022-01-21 16:03:16 -0800

    Adjusted mod tap

--- keyboards/system76/launch_1/keymaps/jemuel-main/keymap.c
+++ keyboards/system76/launch_1/keymaps/jemuel-main/keymap.c
@@ -47,7 +47,7 @@ ________________________________________________________________________________
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,       KC_HOME,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,      KC_PGUP,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      KC_PGDN,
-        MO(2), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,    KC_ENT,           KC_END,
+        MO(2), KC_A, KC_S, LALT_T(KC_D), LCTL_T(KC_F),    KC_G,    KC_H,    RCTL_T(KC_J), LALT_T(KC_K), KC_L, KC_SCLN, KC_QUOT,    KC_ENT,           KC_END,
         KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,    KC_UP,
         KC_LCTL, KC_LGUI, MO(2),   KC_LALT,    LSFT_T(KC_BSPC),    KC_SPC,     KC_RCTL,   KC_RALT,   MO(2),         KC_LEFT, KC_DOWN, KC_RGHT
   ),
@@ -170,4 +170,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     }
     return true;
+}
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LALT_T(KC_S):
+            return TAPPING_TERM + 300;
+        case LCTL_T(KC_F):
+            return TAPPING_TERM + 300;
+        case RCTL_T(KC_J):
+            return TAPPING_TERM + 300;
+        case LALT_T(KC_L):
+            return TAPPING_TERM + 300;
+        default:
+            return TAPPING_TERM;
+    }
 }
