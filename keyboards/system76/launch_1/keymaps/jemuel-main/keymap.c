ac4935bbab98a51144c8ab7bc515ec9d27ae3917
Jemuel Balangcod <jemuelb@gmail.com>
2022-01-24 09:01:28 -0800

    Left spacebar as FN layer

--- keyboards/system76/launch_1/keymaps/jemuel-main/keymap.c
+++ keyboards/system76/launch_1/keymaps/jemuel-main/keymap.c
@@ -42,7 +42,7 @@ ________________________________________________________________________________
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,      KC_PGDN,
         MO(3), KC_A,   KC_S,    KC_D,   KC_F,   KC_G,    KC_H,   KC_J,  KC_K,   KC_L,  KC_SCLN,  KC_QUOT,    KC_ENT,           KC_END,
         KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,    KC_UP,
-        KC_LCTL, KC_LGUI, MO(3),   KC_LALT,    LSFT_T(KC_BSPC),    LT(2, KC_SPC),     KC_RCTL,   KC_RALT,   MO(3),         KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, MO(3),   KC_LALT,    LT(2, KC_BSPC),    LT(2, KC_SPC),     KC_RCTL,   KC_RALT,   MO(3),         KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
     /*
@@ -161,6 +161,8 @@ ________________________________________________________________________________
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LT(4, KC_SPC):
+            return TAPPING_TERM + 200;
+        case LT(2, KC_BSPC):
             return TAPPING_TERM + 200;
         case LSFT_T(KC_BSPC):
             return TAPPING_TERM + 200;
